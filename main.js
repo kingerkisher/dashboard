7a280c1172c4823746e7bc8bb775cdc920d00901
kingerkisher <100521032+kingerkisher@users.noreply.github.com>
2024-07-28 20:34:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var navlink = document.querySelectorAll('.navlink');
+
+navlink.forEach(link => {
+    document.getElementById('dashboard').style.display = 'block';
+    link.addEventListener('click', () => {
+        var page = link.dataset.page;
+        // console.log(page);
+        switch (page) {
+            case 'dashboard': {
+                var dashboard = document.getElementById('dashboard').style.display = 'block';
+                var addnew = document.getElementById('addstock').style.display = 'none';
+                var editstock = document.getElementById('editstock').style.display = 'none';
+                break;
+            }
+            case 'addnew': {
+                var dashboard = document.getElementById('dashboard').style.display = 'none';
+                var addnew = document.getElementById('addstock').style.display = 'block';
+                var editstock = document.getElementById('editstock').style.display = 'none';
+                break;
+            }
+            case 'editstock': {
+                var dashboard = document.getElementById('dashboard').style.display = 'none';
+                var addnew = document.getElementById('addstock').style.display = 'none';
+                var edit = document.getElementById('editstock').style.display = 'block';
+                break;
+            }
+        }
+
+    });
+});
