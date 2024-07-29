513f9b7f9181a7ec3a75e2cbfbc28c2e2b5fdd49
kingerkisher <100521032+kingerkisher@users.noreply.github.com>
2024-07-29 15:44:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,30 +2,26 @@ var navlink = document.querySelectorAll('.navlink');
 
 navlink.forEach(link => {
     document.getElementById('dashboard').style.display = 'block';
-    document.getElementById('addstock').style.display = 'none';
-    document.getElementById('editstock').style.display = 'none';
+
     link.addEventListener('click', () => {
         var page = link.dataset.page;
         // console.log(page);
         switch (page) {
-            case 'dashboard': {
-                var dashboard = document.getElementById('dashboard').style.display = 'block';
-                var addnew = document.getElementById('addstock').style.display = 'none';
+            case 'dashboard': 
+                var dashboard = document.getElementById(page).style.display = 'block';
+                var addnew = document.getElementById('addnew').style.display = 'none';
                 var editstock = document.getElementById('editstock').style.display = 'none';
                 break;
-            }
-            case 'addnew': {
+            case 'addnew':
+                var addnew = document.getElementById(page).style.display = 'block';
                 var dashboard = document.getElementById('dashboard').style.display = 'none';
-                var addnew = document.getElementById('addstock').style.display = 'block';
                 var editstock = document.getElementById('editstock').style.display = 'none';
                 break;
-            }
-            case 'editstock': {
+            case 'editstock':
+                var edit = document.getElementById(page).style.display = 'block';
                 var dashboard = document.getElementById('dashboard').style.display = 'none';
-                var addnew = document.getElementById('addstock').style.display = 'none';
-                var edit = document.getElementById('editstock').style.display = 'block';
+                var addnew = document.getElementById('addnew').style.display = 'none';
                 break;
-            }
         }
 
     });
